487b07d9e582be7304f3e2ce6a1fbd75874abb57
Gayum Sasiri <gayumsasiri.gs@gmail.com>
2024-01-20 21:49:23 +0530

    User api layer implemented

--- src/main/java/com/clancode/labs/api/UserHttpController.java
+++ src/main/java/com/clancode/labs/api/UserHttpController.java
@@ -1,13 +1,51 @@
 package com.clancode.labs.api;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.clancode.labs.services.custom.UserService;
+import com.clancode.labs.to.UserTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
 @CrossOrigin("*")
 public class UserHttpController {
 
-    //todo : implementation of rest controllers
+    private final UserService service;
+
+    public UserHttpController(UserService service) {
+        this.service = service;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    public UserTO createNewUser(@RequestBody @Validated UserTO userTO) {
+        return service.saveUser(userTO);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping(value = "/{user-id}",consumes = "application/json")
+    public void updateUser(@PathVariable("user-id") Integer userId,
+                            @RequestBody @Validated UserTO userTO) {
+        userTO.setUserId(userId);
+        service.updateUser(userTO);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping(value = "/{user-id}")
+    public void deleteUser (@PathVariable("user-id") Integer userId) {
+        service.deleteUser(userId);
+    }
+
+    @GetMapping(value = "/{user-id}", produces = "application/json")
+    public UserTO getUser(@PathVariable("user-id") Integer userId) {
+        return service.getUserDetails(userId);
+    }
+
+    @GetMapping(produces = "application/json")
+    public List<UserTO> getAllUsers() {
+        return service.getUserList();
+    }
 }
