cd488a32a83a2563360ce569ece182296ce230c8
Gayum Sasiri <gayumsasiri.gs@gmail.com>
2024-01-20 08:24:56 +0530

    put handle method and post handle method implemented for task was completed.

--- src/main/java/com/clancode/labs/controllers/TaskController.java
+++ src/main/java/com/clancode/labs/controllers/TaskController.java
@@ -3,23 +3,47 @@ package com.clancode.labs.controllers;
 import com.clancode.labs.model.Task;
 import com.clancode.labs.services.TaskService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/tasks")
 @CrossOrigin("*")
 @AllArgsConstructor
 public class TaskController {
 
     private TaskService taskService;
 
-    @GetMapping(value = "/tasks", produces = "application/json")
+    @GetMapping( produces = "application/json")
     public List<Task> getTask(){
         return taskService.getTasks();
+    }
+
+    @PostMapping(consumes = "multipart/form-data", produces = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Task addTask(@RequestBody Task task) {
+        return taskService.save(task);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> addTask(@RequestBody Task taskPara, @PathVariable Integer id){
+         if(taskService.existById(id)) {
+            Task existingTask = taskService.getTaskById(id).orElseThrow(()-> new EntityNotFoundException("Requested task not found"));
+            existingTask.setTitle(taskPara.getTitle());
+            existingTask.setDescription(taskPara.getDescription());
+            existingTask.setDueDate(taskPara.getDueDate());
+
+            taskService.save(existingTask);
+            return ResponseEntity.ok().body(existingTask);
+         }else {
+             HashMap<String, String> message = new HashMap<>();
+             message.put("message ", id + "task not found or matched");
+             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
+         }
     }
 }
--- src/main/java/com/clancode/labs/services/TaskService.java
+++ src/main/java/com/clancode/labs/services/TaskService.java
@@ -5,7 +5,9 @@ import com.clancode.labs.repository.TaskRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -15,5 +17,17 @@ public class TaskService {
 
     public List<Task> getTasks(){
         return taskRepository.findAll();
+    }
+
+    public Task save(Task task) {
+        return taskRepository.saveAndFlush(task);
+    }
+
+    public boolean existById(Integer id) {
+        return taskRepository.existsById(id);
+    }
+
+    public Optional<Task> getTaskById(Integer id){
+        return taskRepository.findById(id);
     }
 }
